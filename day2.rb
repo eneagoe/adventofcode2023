dc015c5bb1b7e8538d98224f50f28fe87fda1dca
eugen neagoe <eneagoe@gmail.com>
2023-12-02 20:30:39 +0200

    Add solution for 2nd puzzle of day 2
      See https://adventofcode.com/2023/day/2

--- day2.rb
+++ day2.rb
@@ -6,11 +6,20 @@ limits = {
   "blue" => 14
 }
 first = 0
+second = 0
 
 File.open(ARGV[0]).readlines.each do |line|
   /^Game\s+(?<id>\d+):\s+(?<game>.*)$/ =~ line
 
-  valid = game.split(/;/).all? do |round|
+  max = {
+    "red" => 1,
+    "green" => 1,
+    "blue" => 1
+  }
+
+  rounds = game.split(/;/)
+
+  valid = rounds.all? do |round|
     round.split(/,/).all? do |hand|
       /\s*(?<cubes>\d+)\s+(?<color>\w+)/ =~ hand
 
@@ -18,10 +27,20 @@ File.open(ARGV[0]).readlines.each do |line|
     end
   end
 
+  rounds.each do |round|
+    round.split(/,/).all? do |hand|
+      /\s*(?<cubes>\d+)\s+(?<color>\w+)/ =~ hand
+
+      max[color] = [max[color], cubes.to_i].max
+    end
+  end
+
   first += id.to_i if valid
+  second += max.values.reduce(:*)
 end
 
 # first puzzle
 puts first
 
 # second puzzle
+puts second
