bf08a1e676ed296901c5866b134df4b01bf5097a
eugen neagoe <eneagoe@gmail.com>
2023-12-04 22:14:32 +0200

    Add ruby solution for 2nd puzzle of day 4

--- day4.rb
+++ day4.rb
@@ -1,16 +1,24 @@
 #!/usr/bin/env ruby -w
 
 first = 0
+cards = [1] * 500
 
-File.open(ARGV[0]).readlines.each do |line|
+n = File.open(ARGV[0]).readlines.each_with_index do |line, i|
   _, wins, owns = line.split(/[:|]/)
   winners = wins.strip.split(/\s+/).map(&:to_i)
   owned = owns.strip.split(/\s+/).map(&:to_i)
   won = winners & owned
-  first += 2**(won.size - 1) if won.size.positive?
-end
+
+  next unless won.size.positive?
+
+  first += 2**(won.size - 1)
+  (i + 1..i + won.size).each do |j|
+    cards[j] += cards[i]
+  end
+end.size
 
 # first puzzle
 puts first
 
 # second puzzle
+puts cards[...n].sum
