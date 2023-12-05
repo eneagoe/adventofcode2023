af2efded6dd894d3c97345e8b4ed027798d51a41
eugen neagoe <eneagoe@gmail.com>
2023-12-05 21:36:21 +0200

    Add ruby solution for 1st part of day 5

---
+++ day5.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby -w
+
+input = File.open(ARGV[0])
+
+# read seeds
+seeds = input.readline.scan(/\d+/).map(&:to_i)
+mapping = Hash[seeds.zip(seeds)]
+
+7.times do
+  2.times { input.readline }
+  while !input.eof? && ((line = input.readline) !~ /^\s*$/)
+    destination, source, size = line.split(/\s+/).map(&:to_i)
+
+    seeds.each do |seed|
+      mapping[seed] = destination + seed - source if (source...source + size).cover?(seed)
+    end
+  end
+
+  seeds = mapping.values
+  mapping = Hash[seeds.zip(seeds)]
+end
+
+# first puzzle
+puts mapping.values.min
