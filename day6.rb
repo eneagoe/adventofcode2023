587878b432919005ab3544c7f5ca87ad6e8fc754
eugen neagoe <eneagoe@gmail.com>
2023-12-06 21:39:20 +0200

    Add ruby solution for 2nd puzzle of day 6

--- day6.rb
+++ day6.rb
@@ -1,21 +1,29 @@
 #!/usr/bin/env ruby -w
 
 input = File.open(ARGV[0])
-times = input.readline.scan(/\d+/).map(&:to_i)
-distances = input.readline.scan(/\d+/).map(&:to_i)
 
-# first puzzle
-first = times.zip(distances).map do |(t, d)|
+first = input.readline
+racetime = first.gsub(/\D+/, '').to_i
+times = first.scan(/\d+/).map(&:to_i)
+second = input.readline
+record = second.gsub(/\D+/, '').to_i
+distances = second.scan(/\d+/).map(&:to_i)
+
+ways_to_win = lambda do |t, d|
   f = ->(x) { x * x - t * x + d }
 
   delta = t**2 - 4 * d
   x1 = ((t - Math.sqrt(delta)) / 2).ceil
   x2 = ((t + Math.sqrt(delta)) / 2).floor
 
-  x1 += 1 if f.call(x1).zero?
-  x2 -= 1 if f.call(x2).zero?
+  x1 += 1 if f.(x1).zero?
+  x2 -= 1 if f.(x2).zero?
 
   (x1..x2).size
-end.reduce(:*)
+end
 
-puts first
+# first puzzle
+puts times.zip(distances).map { ways_to_win.(*_1) }.reduce(:*)
+
+# second puzzle
+puts ways_to_win.call(racetime, record)
