e43dd9618bc82e63e21a4ea4d24c5e47bd36222e
eugen neagoe <eneagoe@gmail.com>
2023-12-09 20:30:10 +0200

    Add ruby solution for 2nd puzzle of day 9

--- day9.rb
+++ day9.rb
@@ -1,17 +1,28 @@
 #!/usr/bin/env ruby -w
 
 first = 0
+second = 0
 
 File.open(ARGV[0]).readlines.each do |line|
   values = line.split(/\s+/).map(&:to_i)
-  prediction = values[-1]
+  first_prediction = values[0]
+  last_prediction = values[-1]
+  sign = -1
 
   until values.all?(&:zero?)
     values = values.each_cons(2).map { |(a, b)| b - a }
-    prediction += values[-1]
+
+    first_prediction += values[0] * sign
+    last_prediction += values[-1]
+    sign *= -1
   end
 
-  first += prediction
+  first += last_prediction
+  second += first_prediction
 end
 
+# first puzzle
 puts first
+
+# second puzzle
+puts second
