8f3d97d58f0f1cbca2aca19edf3319674a883d83
eugen neagoe <eneagoe@gmail.com>
2023-12-11 23:56:06 +0200

    Add ruby solution for 2nd puzzle of day 11

--- day11.rb
+++ day11.rb
@@ -1,47 +1,50 @@
 #!/usr/bin/env ruby -w
 
 space = []
+empty_lines = []
 
-File.open(ARGV[0]).readlines.each do |line|
+File.open(ARGV[0]).readlines.each_with_index do |line, i|
   c = line.chomp.chars
 
   space << c.dup
-  # expand lines
-  space << c.dup if c.all? { _1 == "." }
+  empty_lines << i if c.all?  { _1 == '.' }
 end
 
 n = space.size
-j = 0
+m = space[0].size
 
 # expand columns
-while j < space[0].size
-  if (0...n).all? { space[_1][j] == "." }
-    (0...n).each do
-      space[_1].insert(j, ".")
-    end
-
-    j += 1
-  end
-
-  j += 1
+empty_cols = (0...m).select do |j|
+  (0...n).all? { space[_1][j] == '.' }
 end
 
-# first puzzle
 first = 0
+second = 0
 m = space[0].size
 
 galaxies = []
 (0...n).each do |i|
   (0...m).each do |j|
-    galaxies << [i, j] if space[i][j] == "#"
+    galaxies << [i, j] if space[i][j] == '#'
   end
 end
 
 galaxies.combination(2).each do |from, to|
+  start = [from[0], to[0]].min
+  stop = [from[0], to[0]].max
   x = (from[0] - to[0]).abs
-  y = (from[1] - to[1]).abs
+  empty_space = empty_lines.count { _1.between?(start + 1, stop - 1) }
+  first += x + empty_space
+  second += x + empty_space * 999_999
 
-  first += x + y
+  y = (from[1] - to[1]).abs
+  start = [from[1], to[1]].min
+  stop = [from[1], to[1]].max
+
+  empty_space = empty_cols.count { _1.between?(start + 1, stop - 1) }
+  first += y + empty_space
+  second += y + empty_space * 999_999
 end
 
 puts first
+puts second
